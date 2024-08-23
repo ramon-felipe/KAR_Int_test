dddc5938688776d1ccce0c49b0f3f7bb77f1ee82
User <user@hackerrank.workspace>
2024-08-23 16:14:30 +0000

    Add initial repository
    Commit by HackerRank

---
+++ dotnet-registration-api/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_registration_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login([FromBody]LoginRequest model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody]RegisterRequest model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Update(int id, [FromBody]UpdateRequest model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet-registration-api/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Data.Repositories;
+using dotnet_registration_api.Helpers;
+using Mapster;
+
+namespace dotnet_registration_api.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> Login(LoginRequest login)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> Register(RegisterRequest register)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> Update(int id, UpdateRequest updateRequest)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
